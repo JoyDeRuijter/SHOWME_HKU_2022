612f297c1923a14d494b1767771085b8255543e7
Joy <joyderuijter@gmail.com>
2022-10-19 18:30:35 +0200

    Add basic player movement to test network-synchronisation, succesfully implement synchronisation

---
+++ Assets/Scripts/PlayerNetwork.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerNetwork : NetworkBehaviour
+{
+    private void Update()
+    {
+        if(!IsOwner)
+            return;
+
+        Vector3 moveDir = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W)) moveDir = Vector3.forward;
+        if (Input.GetKey(KeyCode.A)) moveDir = Vector3.left;
+        if (Input.GetKey(KeyCode.S)) moveDir = Vector3.back;
+        if (Input.GetKey(KeyCode.D)) moveDir = Vector3.right;
+
+        float moveSpeed = 3f;
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+}
