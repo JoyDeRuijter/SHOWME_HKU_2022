b5696f8eeb460dea12ffd3469ff2f7270ee0533a
Eise <eise.booij@gmail.com>
2022-10-30 10:21:58 +0100

    Added Player detection and very basic hiding behaviour

--- Assets/Enemies/EnemyScript.cs
+++ Assets/Enemies/EnemyScript.cs
@@ -2,14 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+//For some reason, the player 'pushes' the enemy away if they enter the sphere collider slowly, but when they enter it quickly, everything works fine
+
 public class EnemyScript : MonoBehaviour
 {
     public GameObject EnemyGun;
     public Transform PlayerAimTarget;
     public float shootingVelocity;
 
+    public float HidingHeightAmount;
+    private bool PlayerInRange;
+
     private void Start()
     {
+        PlayerInRange = false; 
+        transform.Translate(new Vector3(0, HidingHeightAmount, 0));
         StartCoroutine(ShootProjectile());
     }
     private void Update()
@@ -20,11 +29,30 @@ public class EnemyScript : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 6)  //layer for player
+        {
+            PlayerInRange = true;
+            transform.Translate(new Vector3(0, -HidingHeightAmount, 0));
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 6)  //layer for player
+        {
+            PlayerInRange = false;
+            transform.Translate(new Vector3(0, HidingHeightAmount, 0));
+        }
+    }
+
     private IEnumerator ShootProjectile() //shoots a projectile every 4 seconds
     {
-        Debug.Log("Euy");
         yield return new WaitForSeconds(4f);
-        EnemyGun.GetComponent<EnemyGunScript>().Enemy_ShootProjectile();
+        if (PlayerInRange)
+        {
+            EnemyGun.GetComponent<EnemyGunScript>().Enemy_ShootProjectile();
+        }
         StartCoroutine(ShootProjectile());
     }
 }
